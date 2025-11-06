ed144e199293399f59f9c16a7679cfc0d1ece4bb
Vandrangi.Rishita Bhargavi <vrishitabhargavi@gmail.com>
2025-11-06 11:03:02 +0530

    Add files via upload

---
+++ minimum element array example.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,a[10],min;
+	printf("enter n value");
+	scanf("%d",&n);
+	printf("enter array elements");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	min = a[0];
+	for(i=1;i<n;i++)
+	{
+		if(a[i]<min)
+		{
+			min = a[i];
+		}
+	}
+	printf("\n minimum element is :%d",min);
+}
