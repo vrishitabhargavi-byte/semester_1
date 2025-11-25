c5d0d06e652d09dccafe4ab7bb7aae3b67af861f
Vandrangi.Rishita Bhargavi <vrishitabhargavi@gmail.com>
2025-11-25 10:09:03 +0530

    Add files via upload

---
+++ words in a sentence.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	char s[100],i;
+	printf("enter a sentence:\n");
+	fgets(s,sizeof(s),stdin);
+	printf("words in a sentence are:\n");
+	for(i=0;s[i]!='\0';i++)
+	{
+		if(s[i]==' ')
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf("%c",s[i]);
+		}
+	}
+}
